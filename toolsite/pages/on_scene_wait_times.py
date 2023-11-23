e9fe7afd97d1dea39d5c3888419f28c35cb33101
PlebbyG <plebeiusgaragicus@gmail.com>
2023-11-23 15:43:16 -0800

    progress - pivot table

--- toolsite/pages/on_scene_wait_times.py
+++ toolsite/pages/on_scene_wait_times.py
@@ -45,6 +45,8 @@ When we load the datafile, we ensure necessary columns exist and are of the corr
 
 Next, we clean the data by removing rows that are not relevant to the analysis.
 
+:warning: R31 and E31 are cross-staffed between agencies, but have inconsidtent/non-matching 'Station Jurisdiction' values depending on the day.  This is a problem and will sway results slightly.
+
 """
 
 
@@ -114,6 +116,8 @@ def process_data(uploaded_file, wait_time, delimiter):
     
     data = drop_and_arrange(data)
 
+    incident_resp = create_incident_responses(data)
+
 
 
 ##############################################################################
@@ -172,36 +176,134 @@ def load_validate(file, delimiter):
 #
 def drop_and_arrange(df: pd.DataFrame) -> pd.DataFrame:
 
-    df = df[df["Activity Type"] != "M"] # messages
-    df = df[df["Activity Type"] != "C"] # more messages?
-    df = df[df["Activity Type"] != "L"] # LOGOFF remarks (duplicative - use #TODO for actual logoff tracking)
-    df = df[df["Activity Type"] != "S"] # "signal" (eg. AUTO RADIO ALERTED PERSONNEL: MD208515 -  (22824))
-
-    # with st.expander("Rows with Activity Type: M, C, L, S are dropped"):
-        # st.write(df.head(n=20))
-
-    #NOTE: this step works to both re-arrange AND drop columns not listed
-    df = df[[
-        "Activity Date",
-        "Apparatus ID",
-        "Apparatus Status",
-        "Activity Code",
-        "Call Number",
-        "Call Type",
-        "Remarks"
-    ]]
-
-    # sort by 'Activity Date' column
-    df = df.sort_values(by=['Activity Date'])
-
-    # reset index
-    df = df.reset_index(drop=True)
-
+    # NOTE: In order to keep the code clean, we will use a "with" statement on the whole section in order to keep data manipulation steps together with explanation text.
     with st.expander("Show cleaned data"):
         st.write(":arrow_forward: Rows with Activity Type: M, C, L, S are dropped")
-        st.write(":arrow_forward: Dropping un-needed columns and re-arranging the rest")
+        df = df[df["Activity Type"] != "M"] # messages
+        df = df[df["Activity Type"] != "C"] # more messages?
+        df = df[df["Activity Type"] != "L"] # LOGOFF remarks (duplicative - use #TODO for actual logoff tracking)
+        df = df[df["Activity Type"] != "S"] # "signal" (eg. AUTO RADIO ALERTED PERSONNEL: MD208515 -  (22824))
+
+
+        st.write(":arrow_forward: Only keep 'RP' and 'MD' rows")
+        # keep rows with "Station Jurisdiction" == "RP" or "MD"
+        df = df[df["Station Jurisdiction"].isin(["RP", "MD"])]
+
+        st.write(":arrow_forward: Drop BLS units")
+        # drop "station" column with "715B" values
+        df = df[df["Station"] != "715B"]
+
+        st.write(":arrow_forward: Create an 'Incident Number' column from 'Call Jurisdiction' and 'Call Number' columns")
+        # incident number column should combine "Call Jurisdiction" and "Call Number" columns and ensure call number has leading zeros to make it 8 digits
+        df["Call Number"] = df["Call Number"].astype(str).str.zfill(8)
+        df["Incident Number"] = df["Call Jurisdiction"] + df["Call Number"]
+        # df = df.drop(columns=["Call Jurisdiction", "Call Number"])
+
+        st.write(":arrow_forward: Drop un-needed columns and rearrange remaining columns")
+        df = df[[
+            "Activity Date",
+            "Incident Number",
+            "Apparatus ID",
+            "Apparatus Status",
+            "Activity Code",
+            "Call Type",
+            "Station Jurisdiction",
+            "Remarks",
+            "Call Jurisdiction",
+            "Call Number"
+        ]]
+
+
+        st.write(":arrow_forward: Sort by 'Activity Date' column")
+        # sort by 'Activity Date' column
+        df = df.sort_values(by=['Activity Date'])
+
+        st.write(":arrow_forward: Reset the index")
+        # reset index
+        df = df.reset_index(drop=True)
+
         # st.write(df.head(n=20))
-        # st.write(df.head())
         st.write(df)
 
+        # st.stop()
+
     return df
+
+
+def create_incident_responses(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a dataframe of incident responses.
+
+    Args:
+        df (pandas.DataFrame): The dataframe to create incident responses from.
+
+    Returns:
+        pandas.DataFrame: The incident responses.
+    """
+
+    # Creating a pivot table with 'Incident Number' as index and combinations of 'Station Jurisdiction' and 'Apparatus Status' as columns
+    pivot_columns = df['Station Jurisdiction'] + ' ' + df['Apparatus Status']
+    pivot_table = df.pivot_table(index='Incident Number',
+                                    columns=pivot_columns,
+                                    values='Activity Date',
+                                    aggfunc='min')
+
+    # Resetting index to make 'Incident Number' a column and renaming the columns for clarity
+    pivot_table.reset_index(inplace=True)
+    pivot_table.columns.name = None  # Remove the name of the columns level
+
+    # Filtering for the specified columns
+    filtered_columns = ['Incident Number', 'RP DP', 'RP ER', 'RP OS', 'RP AV', 'MD DP', 'MD ER', 'MD OS']
+    final_incident_response_df = pivot_table[filtered_columns]
+
+    # Filtering the dataframe to include only those rows with at least one RP and one MD response
+    rp_columns = ['RP DP', 'RP ER', 'RP OS', 'RP AV']
+    md_columns = ['MD DP', 'MD ER', 'MD OS']
+
+    # Applying the filter
+    filtered_incident_df = final_incident_response_df.dropna(subset=rp_columns, how='all').dropna(subset=md_columns, how='all')
+
+    filtered_incident_df = filtered_incident_df[ filtered_incident_df['Incident Number'] != ' 0000<NA>']
+    #  df[df["Activity Type"] != "S"]
+
+    # Display the first few rows of the final incident response dataframe
+    filtered_incident_df.head()
+
+    with st.expander("Show final incident response dataframe"):
+        st.write(filtered_incident_df)
+
+    return filtered_incident_df
+
+
+
+
+
+
+
+prompts = """
+I need to take this datafile of 911 system apparatus status updates, and convert it into a dataframe of "incident responses."
+
+Each row in the incident response dataframe should have a unique incident number as well as the earliest timestamp for each "Station Jurisdictions" status.
+
+RP = portland fire apparatus
+MD = AMR ALS ambulance
+
+DP - dispatched
+ER - en route
+OS - on scene
+OG - staged nearby (consider same as on scene)
+TR - transporting ( ambulance only )
+TC - transport complete (at hospital)
+AV - available in service (call cleared)
+IQ - in quarters (at station, available for calls)
+
+---
+
+The resulting dataframe should have columns of "call number" "RP DP" "RP ER" "RP OS" "RP AV" "MD DP" "MD ER" "MD OS"
+
+Each row should have a unique call number.  As in, each status update should be summarized into a single row representing a single call in its entirety
+
+---
+
+This looks great so far.  Let's add it to by removing every call number row where there is no RP response or no MD response.  In other words, I only want to show rows that include BOTH RP and MD units.
+"""
